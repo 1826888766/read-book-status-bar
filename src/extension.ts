b0bcf59f9014173645c8023afa66a5f0340c31aa
马良 <1826888766@qq.com>
2020-12-15 09:18:11 +0800

    删除书架文件

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,6 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { Log } from "./util/log";
-import { BookShelf } from "./util/bookshelf";
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
