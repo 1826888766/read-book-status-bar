6565f4556dd73c94bbe2d5bbe793a05e4f7cec82
马良 <1826888766@qq.com>
2022-07-29 16:09:50 +0800

    导入

--- src/commands/import.ts
+++ src/commands/import.ts
@@ -1,17 +1,22 @@
 import { ReadBook } from "../main";
 import { commands, window, StatusBarAlignment, StatusBarItem } from "vscode";
-var handler: ReadBook,importStatusBarItem: StatusBarItem;
+var handler: ReadBook, importStatusBarItem: StatusBarItem;
 
 function _import() {
     let command = "read-book-status-bar.import";
-    importStatusBarItem = window.createStatusBarItem(StatusBarAlignment.Right,60);
+    importStatusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 60);
     importStatusBarItem.text = '$(add) 导入';
     importStatusBarItem.tooltip = '导入';
     importStatusBarItem.command = command;
     importStatusBarItem.show();
     handler.context.subscriptions.push(importStatusBarItem);
     commands.registerCommand(command, () => {
-       
+        window.showOpenDialog({
+            title: "选择书籍txt",
+            filters: {
+                'file': ['txt']
+            }
+        });
     });
 }
 // 初始化阅读控制
